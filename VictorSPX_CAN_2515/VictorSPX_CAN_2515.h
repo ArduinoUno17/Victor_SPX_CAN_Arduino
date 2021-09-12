b18588d3a3f7610ff064070c9514147e1dbbe8fb
Joshua Newport <Joshua.newport10@gmail.com>
2021-09-11 21:09:43 -0400

    Removed Unnecessary Items
    What do you think?

--- VictorSPX_CAN_2515/VictorSPX_CAN_2515.h
+++ VictorSPX_CAN_2515/VictorSPX_CAN_2515.h
@@ -32,16 +32,8 @@ public:
 private:
 	MCP2515 *CAN;
 	int startCAN(void);
-	//unsigned char enableFrame[8] = {0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 	struct can_frame MotorFrame;
 	struct can_frame enableFrame;
-	//unsigned char disableFrame[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-	// bool disabled = true;
-	// long _arbID;
-	// long _VictorSPXarbID;
-	// long _enableArbID;
-	// long _PrcntOutputFrame;
-	// int _MCP2515_CS_PIN;
 	long _arbID = 0x00000000;
 	long _VictorSPXarbID = 0x01040000;
 	long _enableArbID = 0x000401BF;
@@ -49,7 +41,6 @@ private:
 	int _MCP2515_CS_PIN = 10;
 	unsigned char Braking[63];			// Keep Track of Which Motors are Using Braking
 	float MtrSpeeds[63];		// Keep Track of Each Motor Speed
-	//unsigned char DeviceIDs[64]; 
 };
 
 #endif
